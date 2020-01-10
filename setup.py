745d9d40bae9b742369be8511236fa45528f28ba
Marcello Puligheddu <33704456+marcelloPuligheddu@users.noreply.github.com>
2020-01-10 16:39:25 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+from setuptools import find_packages
+
+exec(open("src/versions.py").read())
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name=__name__,
+      version=__version__,
+      packages=find_packages(),
+      description=__description__,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url=__url__,
+      author=__author__,
+      author_email=__author_email__,
+      license=__license__,
+      include_package_data=True,
+      install_requires=[
+          'pandas',
+          'mpi4py',
+          'numpy',
+          'scipy',
+          'datetime',
+          'setuptools',
+          'beautifultable',
+          'sphinx', 
+          'sphinx_rtd_theme',
+          'nbsphinx', 
+          'ipython', 
+          'pyaml', 
+          'mendeleev',
+          'urllib3' 
+      ],
+      python_requires='>=3.6, <4')
